852735b3424cc6bf59449d6cd49632b3c5dd00e7
toothbrus <752324621@qq.com>
2024-09-03 15:49:01 +0800

    the second

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -7,13 +7,12 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        if (a >= b) {
+            return a;
+        }
+        else {
+            return b;
+        }
     }
 
 
@@ -22,17 +21,8 @@ public class DebugExercise2 {
         int x = a, y = b;
         /* If you're stepping into this function, click the
            step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        int result = x + y;
+        return result;
     }
 
     /** Returns a new array where entry i is the max of
